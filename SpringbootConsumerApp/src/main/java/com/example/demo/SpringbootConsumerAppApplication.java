6c0fec15ddeb42b00b7ad62c6ece45634882a2f6
rameshgorla532 <rameshgorla532@gmail.com>
2022-03-24 13:16:31 +0530

    SpringbootConsumerApp first commit

---
+++ SpringbootConsumerApp/src/main/java/com/example/demo/SpringbootConsumerAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootConsumerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootConsumerAppApplication.class, args);
+	}
+
+}
---
+++ SpringbootConsumerApp/src/test/java/com/example/demo/SpringbootConsumerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConsumerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
