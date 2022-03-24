d2f57ca19e2aa43280bbb6017b1f667064c55431
rameshgorla532 <rameshgorla532@gmail.com>
2022-03-24 14:43:35 +0530

    SpringBootPRovider Intial Commit

---
+++ SpringBootEurekaProvider/src/main/java/com/example/demo/controller/ProviderController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/provider")
+public class ProviderController {
+	
+	@GetMapping("/get")
+	public ResponseEntity<String> getShow()
+	{
+		return new ResponseEntity<String>("Hello Provider!!",HttpStatus.OK);
+	}
+
+}
